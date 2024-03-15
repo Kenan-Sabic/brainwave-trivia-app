5a7c396a4814cc49171651e301310d7ae9bee712
Dalila Salcin <dalilasalcin@hotmail.com>
2024-03-15 01:33:54 +0100

    GameOver screen, not finished

--- client/screens/GameOverScreen.js
+++ client/screens/GameOverScreen.js
@@ -1,12 +1,61 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useNavigation } from '@react-navigation/native';
 
-const GameOverScreen = () => {
+
+export default function GameOverScreen() {
+  let [fontsLoaded] = useFonts({
+    
+    'Orbitron-Bold': require('./assets/fonts/Orbitron-Bold.ttf'),
+    'Monofett-Regular': require('./assets/fonts/Monofett-Regular.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  const navigation = useNavigation();
+
   return (
     <View>
-        <Text>Game Over Screen</Text>
-    </View>
-  )
-}
+      <ImageBackground source={require('./assets/images/background1.png')} style={styles.background}>
+        <View style={styles.container}>
+          <View style={styles.banner}>
+            <Text style={styles.bannerText}>BRAINWAVE</Text>
+          </View>
+          
+          <Text>Game Over!</Text>
+        </View>
+      </ImageBackground>
+    </View> 
+  );
+};
 
-export default GameOverScreen
+const styles = StyleSheet.create({
+  //Add fonts, add score, return button, and logo to the banner !! 
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    height: '100%',
+    width: '100%',
+  },
+  banner: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#6EBFBB",
+    paddingVertical: 50,
+    //paddingHorizontal: 20,
+    alignItems: 'center',
+   //  flexDirection:"row", 
+  },
+  bannerText: {
+    marginTop: 25,
+    fontSize: 24,
+    //fontWeight: 'bold',
+    color: 'black', 
+  },
+});
