06971dae1e856fb020513af8aec59b99afe15aa0
Kenan-Sabic <kenansabipsn@gmail.com>
2024-04-01 00:27:47 +0200

    Made the web header into standalone component

--- client/screens/components/HeaderLargeScreen.js
+++ client/screens/components/HeaderLargeScreen.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { View, ImageBackground, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, Pressable  } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 
 
 export default function HeaderLargeScreen (){
+    const navigation = useNavigation();
+    let [fontsLoaded] = useFonts({
+    
+        'Orbitron-Bold': require('../assets/fonts/Orbitron-Bold.ttf'),
+        'Monofett-Regular': require('../assets/fonts/Monofett-Regular.ttf'),
+      });
+      if (!fontsLoaded) {
+        return null;
+      }
 
     return(
-        <View style={styles.header}>
-
-        <Text>HEADER</Text>
-
-
-        </View>
+        <View style={{flexDirection: 'row', alignItems: 'center', backgroundColor:'#6EBFBB'}}>
+        <Image source={require('../assets/images/brain.svg')}/>
+        <Text style={styles.title1Desk}>BRAINWAVE</Text>
+        <Pressable><Text style={styles.navigationPressable}  onPress={() => navigation.navigate('Register')}>Home</Text></Pressable>
+      </View>
 
 
     )
@@ -23,14 +32,18 @@ const {width,height}= Dimensions.get('window');
 const isLargeScreen = width>=768;
 
 const styles = StyleSheet.create({
-    header:{
-        display:isLargeScreen ? block : none,
-        width:"100vw",
-        backgroundColor:'#4B8F8C',
-        position:'absolute',
-        top:0,
-        left:0,
-    }
+    title1Desk: {
+        color: 'white',
+        fontSize:55,
+        fontFamily: 'Monofett-Regular'
+      },
+      navigationPressable:{
+        marginHorizontal:5,
+        color:'white',
+        fontFamily:'Orbitron-Bold',
+        fontSize:25,
+   
+      }
 
 
 })
